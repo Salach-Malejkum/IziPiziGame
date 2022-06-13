97ba02a2ca80613aeee8810890bb45c076b46dc4
Topaz-Frog <erwinos753@gmail.com>
2022-06-14 01:21:04 +0200

    Revert "sex na scenie"
    This reverts commit a9664d1fb1e3b98b10ad3193a99c3ba1f4b444c8.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,15 +8,9 @@ using Random = UnityEngine.Random;
 public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-    private Animator animator;
-    private AudioSource audio;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
     public float hp;
-
-    [SerializeField]
-    public AudioClip[] clips;
-    private bool soundplayed = false;
 
     //Attack
     public float timeBetweenAttacks;
@@ -37,8 +31,6 @@ public class EnemyAI : MonoBehaviour
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-
     }
 
     private void Update()
@@ -95,7 +87,6 @@ public class EnemyAI : MonoBehaviour
         {
             attacked = true;
             player.GetComponent<PlayerScript>().LoseHP();
-            animator.SetBool("IsAttacking", true);
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
     }
@@ -109,14 +100,8 @@ public class EnemyAI : MonoBehaviour
     {
         hp -= damage;
         wasAttacked = true;
-        int pickedClip = Random.Range(0, clips.Length);
-        audio.PlayOneShot(clips[pickedClip]);
 
-        if (hp <= 0)
-        {
-            animator.SetBool("isDead", true);
-            Invoke(nameof(Die), .5f);
-        }
+        if (hp <= 0) Invoke(nameof(Die), .5f);
     }
 
     private void Die()
